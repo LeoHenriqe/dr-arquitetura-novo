51803538a807094119204dc4a87ae5b3d5e4a8b7
Leonardo Henrique <90967601+LeoHenriqe@users.noreply.github.com>
2024-06-07 19:29:03 -0300

    up

--- js/projetos.js
+++ js/projetos.js
@@ -1,5 +1,5 @@
 const projects = [
-    ["/imagens/FARMÁCIA BOM PREÇO/1.jpg", "/imagens/FARMÁCIA BOM PREÇO/2.jpeg", "/imagens/FARMÁCIA BOM PREÇO/3.jpeg", "/imagens/FARMÁCIA BOM PREÇO/4.jpg"],
+    ["/imagens/FARMÁCIA BOM PREÇO/1.png", "/imagens/FARMÁCIA BOM PREÇO/2.png", "/imagens/FARMÁCIA BOM PREÇO/3.png", "/imagens/FARMÁCIA BOM PREÇO/4.png"],
     ["/imagens/PROJETO HABITAÇÃO SERIADA/1.png", "imagens/PROJETO HABITAÇÃO SERIADA/2.png"],
     ["/imagens/studiok/SVG/Ativo 1.svg", "/imagens/studiok/SVG/Ativo 2.svg", "/imagens/studiok/SVG/Ativo 3.svg"],
     ["imagens/j231/1.jpg", "imagens/j231/2.png", "imagens/j231/3.png", "imagens/j231/4.png", "imagens/j231/5.png"],
